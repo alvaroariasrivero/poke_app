13f4a572c5752528c89551c978907563c109f736
Álvaro Arias Rivero <alvaroariasrivero92@gmail.com>
2021-12-20 15:50:45 +0100

    impresión lista pokemon

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Card = ({pokemon}) => {
   return <div>
-    <p>{pokemon.name}</p>
+    <p>Name: {pokemon.name}</p>
     <img src={pokemon.picture} alt={pokemon.name} />
   </div>;
 };
