59b2b669863ed61c3d6fa294d60d92472de112bd
Álvaro Arias Rivero <alvaroariasrivero92@gmail.com>
2021-12-21 18:55:52 +0100

    routing comenzado

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import {Routes, Route} from 'react-router-dom';
 import Form from '../Form/Form';
 import ListaPokemon from '../ListaPokemon/ListaPokemon';
 
@@ -6,8 +7,10 @@ import ListaPokemon from '../ListaPokemon/ListaPokemon';
 const Main = () => {
   
   return <main>
-        <Form/>
-        <ListaPokemon/>
+      <Routes>
+        <Route path='/' element={<ListaPokemon/>}/>
+        <Route path='/search' element={<Form/>}/>
+      </Routes>
     </main>;
 };      
 
--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,7 +1,13 @@
 import React from "react";
+import {Link} from 'react-router-dom';
 
 const Nav = () => {
-  return <div>Nav</div>;
+  return <nav>
+    <ul>
+      <Link to='/'>PokéList</Link>
+      <Link to='/search'>PokéSearch</Link>
+    </ul>
+  </nav>;
 };
 
 export default Nav;
